49cdad50da9fd0b1736e730b5c67becb4ae48a83
Aditi-018 <aditisahu.in@gmail.com>
2026-06-10 23:31:17 +0530

    [Aditi]add:Control flow codes Level1,2 and 3

---
+++ Core-Java-Practice/gcr-codebase/control-flow/level 1/LargestNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LargestNumber{
+	public static void main(String[] args){
+	Scanner sc= new Scanner (System.in);
+	System.out.println("write 1st no");
+	int n1=sc.nextInt();
+	System.out.println("write 2nd no");
+	int n2=sc.nextInt();
+	System.out.println("write 3rd no");
+	int n3=sc.nextInt();
+	if(n1>=n2 && n1>=n3){
+	System.out.println("the first no is largest");
+	}
+	else if(n2>=n1 && n2>n=3){
+	System.out.println("the second no is largest");
+	}
+	else if(n1==n2 && n2==n3){
+		System.out.println("All are equal");
+	}
+	else
+	System.out.println("the third no is largest");
+	}
+	}
+	
